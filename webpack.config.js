8bafa3a7ce91d0509303274efcc2bc74013a97b9
andrei <andrei.fzto@gmail.com>
2016-02-18 18:57:49 +0300

    rename app to src

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ process.env.BABEL_ENV = TARGET;
 
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
+  app: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
 };
 // точка входа
